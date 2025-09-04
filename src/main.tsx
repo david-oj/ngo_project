d8e6ab10f60beac404fce35d2d39da8328e2d9b0
David-A-Ojo <davidojo.dev@gmail.com>
2025-09-04 13:09:23 +0100

    feat: add Inter and Lato fonts via Fontsource

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "@fontsource/inter/900.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/600.css";
+
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/400-italic.css";
+import "@fontsource/lato/700.css";
+import "@fontsource/lato/900.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
